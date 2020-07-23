919a44a288872f31d2f7051fcf2cdd019201fae0
Giacomo Miolo <giacomo.miolo@gmail.com>
2020-07-23 13:10:04 +0200

    Added custom callback

--- load_data.py
+++ load_data.py
@@ -1,2 +1,10 @@
 import tensorflow as tf
 print(tf.__version__)
+
+class ShowTime(Callback):
+    
+    def on_train_begin(self, logs=None):
+        """Docstring"""
+        print(f"Training started at: {datetime.datetime.now().time()}")
+    def on_train_end(self, logs=None):
+        print(f"Training ended at: {datetime.datetime.now().time()}")
